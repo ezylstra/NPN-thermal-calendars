9874b5f1efda5944659d01e9735e792389cfa47b
Erin Zylstra <erinzylstra@gmail.com>
2025-03-21 15:47:39 -0700

    Assess new mean-sd-year count rasters

--- count-years-exploration.R
+++ count-years-exploration.R
@@ -1,5 +1,8 @@
-
+library(dplyr)
 library(terra)
+library(ggplot2)
+library(tidyterra)
+library(cowplot)
 
 # Load file that lists summary statistics and thresholds
 params <- read.csv("shiny-app/parameters.csv")
@@ -19,13 +22,76 @@ for (i in 1:length(thresholds)) {
                                     thresholds[i], ".tiff"))
 }
 
-thresh <- 1
-comb <- c(means_list[[thresh]], sds_list[[thresh]], counts_list[[thresh]])
-plot(comb)
+# First, look at rasters and see whether there are any obvious issues
+# (Note: this takes a few minutes)
+for (thresh in 1:50) {
+  comb <- c(means_list[[thresh]], sds_list[[thresh]], counts_list[[thresh]])
+  comb[[3]] <- as.factor(comb[[3]])
+  title_gg <- ggplot() +
+    labs(title = paste0("Threshold: ", thresholds[thresh], " degF"))
+  m <- ggplot() +
+    geom_spatraster(data = comb[[1]], aes(fill = mean)) +
+    scale_fill_viridis_c(name = "Mean")
+  sd <- ggplot() + 
+    geom_spatraster(data = comb[[2]], aes(fill = sd)) + 
+    scale_fill_viridis_c(name = "SD")
+  count <- ggplot() +
+    geom_spatraster(data = comb[[3]], aes(fill = count)) +
+    scale_fill_viridis_d(name = "No. years")
+  plots <- plot_grid(m, sd, count, nrow = 2)
+  plot(plot_grid(title_gg, plots, ncol = 1, rel_heights = c(0.1, 1)))
+}
 
+# Look at values in one set of rasters
 thresh <- 50
+thresholds[thresh]
 comb <- c(means_list[[thresh]], sds_list[[thresh]], counts_list[[thresh]])
-plot(comb)
-plot(comb[[3]] == 29)
+df <- data.frame(comb)
 
+head(df)
+df <- df %>%
+  mutate(mean2 = case_when(
+    mean == Inf ~ "Inf",
+    is.na(mean) ~ "NA",
+    .default = "value"
+  )) %>%
+  mutate(sd2 = case_when(
+    is.na(sd) ~ "NA",
+    sd == 0 ~ "0",
+    .default = "value"
+  )) %>%
+  mutate(count2 = case_when(
+    count == 0 ~ "0",
+    count %in% 1 ~ "1",
+    count %in% 2:30 ~ "2:30",
+  ))
 
+count(df, mean2, sd2, count2)
+# mean = NA, SD = NA, count = 0: outside area of interest 
+# mean = Inf, SD = NA, count = 0: cell reached threshold in 0 years
+# mean = value, SD = NA, count = 1: cell reached threshold in 1 year
+# mean = value, SD = 0, count > 1: cell reached same threshold every year it was reached
+# mean = value, SD = value, count > 1: cells reached different thresholds multiple years
+
+# Create a new raster where all means with year count == 1 converted to NA
+comb2 <- comb
+comb2[[1]][comb2[[3]] == 1] <- NA
+df2 <- data.frame(comb2) %>%
+  mutate(mean2 = case_when(
+    mean == Inf ~ "Inf",
+    is.na(mean) ~ "NA",
+    .default = "value"
+  )) %>%
+  mutate(sd2 = case_when(
+    is.na(sd) ~ "NA",
+    sd == 0 ~ "0",
+    .default = "value"
+  )) %>%
+  mutate(count2 = case_when(
+    count == 0 ~ "0",
+    count %in% 1 ~ "1",
+    count %in% 2:30 ~ "2:30",
+  ))
+count(df2, mean2, sd2, count2)
+
+# Everything looks good.
