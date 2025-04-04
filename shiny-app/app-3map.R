30c5d3129597b538a6a364573bc4e49696343112
Erin Zylstra <erinzylstra@gmail.com>
2025-04-04 14:48:38 -0700

    App with 3 maps, threshold selection by pest spp

--- shiny-app/app-3map.R
+++ shiny-app/app-3map.R
@@ -1,13 +1,12 @@
-# App to display rasters with mean and current day-of-year that AGDD thresholds 
-# are reached in the northeastern US
-
-# ER Zylstra
-# 1 Apr 2025
+# Shiny app to explore when AGDD thresholds are met in the northeastern US: 
+# Mean day of the year (and SD) over 30-yr period as well as day in current year
 
 library(lubridate)
+library(dplyr)
 library(raster)
 library(terra)
 library(shiny)
+library(shinyjs)
 library(leaflet)
 library(leafem)
 library(htmlwidgets)
@@ -31,18 +30,23 @@ doys <- terra::unwrap(doys)
 doys <- raster::brick(doys)
 names(doys) <- stringr::str_replace_all(names(doys), "X", "t")
 
-# Load file that lists summary statistics and thresholds
-params <- read.csv("parameters.csv")
+# List of thresholds
+threshold_list <- seq(50, 2500, by = 50)
+
+# Load file with pest-specific thresholds
+pests <- read.csv("pest-thresholds-subset.csv")
+pests <- pests %>%
+  mutate(layer = paste0("t", threshold))
 
 # Load static mean/SD rasters 
 mean_sd <- brick("mean-sd-brick.tif")
 
 # Create mean RasterBrick 
 means <- mean_sd[[1:50]]
-names(means) <- names(doys)
+names(means) <- paste0("t", threshold_list)
 # Create SD RasterBrick
 sds <- mean_sd[[51:100]]
-names(sds) <- names(doys)
+names(sds) <- paste0("t", threshold_list)
 
 # Mean, SD, and current-year rasters projected for Leaflet already (epsg:3857):
 # +proj=merc +a=6378137 +b=6378137 +lat_ts=0 +lon_0=0 +x_0=0 +y_0=0 +k=1 
@@ -64,32 +68,42 @@ myLabelFormat <- function(..., dates = FALSE){
 # ui --------------------------------------------------------------------------#
 
 ui <- page_fillable(
-  
-  theme = bslib::bs_theme(bootswatch = "sandstone"),
-  
+
+  useShinyjs(),
+
   titlePanel("What day of the year do locations in the northeastern U.S. reach 
              accumulated heat thresholds?"),
   
   layout_columns(
-    navset_tab(
-      nav_panel("Select threshold",
-                layout_columns(
-                  selectInput(inputId = "threshold", 
-                              label = "AGDD threshold (deg F):", 
-                              choices = unique(params$threshold)),
-                  sliderInput(inputId = "opacity",
-                              label = "Opacity:",
-                              min = 0,
-                              max = 1, 
-                              value = 0.8),
-                  p("Zoom in and click on the map to identify the mean day of the 
-                  year (and SD) for a specific location. Value will appear in 
-                  the lower left corner of the map."),
-                  col_widths = c(8, 4, 12),
-                )
+    navset_card_tab(
+      nav_panel("Settings",
+        layout_columns(
+          p("Select a threshold directly or select a pest species, event and 
+            threshold type. Click on the Plot button to display maps."),
+          radioButtons("method", "Selection method",
+                       choices = c("Threshold" = "Threshold", "Pest" = "Pest")),
+          selectInput(inputId = "threshold", 
+                      label = "AGDD threshold:", 
+                      choices = unique(threshold_list)),
+          actionButton(inputId = "plot",
+                       label = "Plot", class = "btn btn-primary"),
+          sliderInput(inputId = "opacity",
+                      label = "Opacity:",
+                      min = 0,
+                      max = 1, 
+                      value = 0.8),
+          selectInput(inputId = "pest",
+                      label = "Species",
+                      choices = unique(pests$spp)),
+          selectInput("event", "Biological event", choices = NULL),
+          selectInput("type", "Threshold type", choices = NULL),
+          col_widths = c(12, 2, 4, -4, 2, 2, 4, 4, 2),
+          row_heights = c(1, rep(4, 4), rep(4, 4))
+        )
       ),
       nav_panel("Methods",
-                p("For each year from 1991-2020, we obtained daily minimum and maximum
+                p(strong("Mean day of year and standard deviation (SD) maps: "), 
+                  "For each year from 1991-2020, we obtained daily minimum and maximum
                   temperatures for the northeastern US at 4-km resolution from the
                   PRISM Climate Group (Oregon State University, ",
                   a(href = "https://prism.oregonstate.edu",
@@ -100,37 +114,31 @@ ui <- page_fillable(
                   reached. The maps display the mean day of the year each threshold was
                   reached (or the standard deviation, SD) over the 30-year period. We
                   excluded any locations where the threshold was reached in only one of
-                  the 30 years.")
-      )
+                  the 30 years."),
+                p(strong("Current day of year map: "),
+                  "Map displays the day of the current year the selected AGDD threshold 
+                  was reached. Daily minimum and maximum temperatures for each day of the
+                  current year were obtained from USA-NPN (link) based on hourly 
+                  data from NCEP (info). These data were then used to calculate daily 
+                  AGDD values (through yesterday) using the Baskerville-Emin method 
+                  with a base temperature of 50 deg F.")
+      ),
     ),
     card(card_header(textOutput(outputId = "doy_header")), leafletOutput("doy")),
     card(card_header("Mean day of year"), leafletOutput("mean")),
     card(card_header("Standard deviation"), leafletOutput("sd")),
     col_widths = c(12, 4, 4, 4),
-    row_heights = c(1, 3, 3, 3)
+    row_heights = c(2, 3, 3, 3)
+    # row_heights = c(1, 3, 3, 3)
 
   ) # end layout_columns
 ) # end page_fillable
 
 # server ----------------------------------------------------------------------#
 
-server <- shinyServer(function(input, output) {
+server <- shinyServer(function(input, output, session) {
   
-  reacMean <- reactive({means[[paste0("t", input$threshold)]]})
-  reacSD <- reactive({sds[[paste0("t", input$threshold)]]})
-  reacDOY <- reactive({doys[[paste0("t", input$threshold)]]})
-  
-  # Logical, where TRUE indicates that all cells in current-year raster are NA 
-  # (i.e., no locations have reached this threshold yet)
-  reacDOY_NA <- reactive({ifelse(ncell(reacDOY()) == freq(reacDOY(), value = NA),
-                                 TRUE, FALSE)})
-  
-  output$doy_header <- renderText({
-    ifelse(reacDOY_NA() == TRUE,
-           "Current day of year - NO LOCATIONS REACHED THRESHOLD YET",
-           "Current day of year")
-  })
-  
+  # Create base maps
   output$mean <- renderLeaflet({
     leaflet() %>%
       fitBounds(lng1 = -88, lat1 = 35, lng2 = -65, lat2 = 47) %>%
@@ -182,7 +190,7 @@ server <- shinyServer(function(input, output) {
           }
         ")
   })
-
+  
   output$doy <- renderLeaflet({
     leaflet() %>%
       fitBounds(lng1 = -88, lat1 = 35, lng2 = -65, lat2 = 47) %>%
@@ -209,71 +217,104 @@ server <- shinyServer(function(input, output) {
         ")
   })
   
-  observe({
+  # Create choices for Event input
+  pest_tab <- reactive({
+    filter(pests, spp == input$pest)
+  })
+  observeEvent(pest_tab(), {
+    choices <- unique(pest_tab()$event)
+    updateSelectInput(inputId = "event", choices = choices)
+  })
+  
+  # Create choices for Type input
+  event_tab <- reactive({
+    filter(pest_tab(), event == input$event)
+  })
+  observeEvent(event_tab(), {
+    choices <- unique(event_tab()$type)
+    updateSelectInput(inputId = "type", choices = choices)
+  })
+  
+  # Enable/disable inputs depending on selection method (Pest/Threshold)
+  observeEvent(input$method, {
+    if(input$method == "Pest") {
+      disable("threshold")
+      enable("pest")
+      enable("event")
+      enable("type")
+    } else {
+      enable("threshold")
+      disable("pest")
+      disable("event")
+      disable("type")
+    }
+  })
+
+  # Execute after clicking Plot button
+  observeEvent(input$plot, {
+    req(input$method)
+    if (input$method == "Pest") {
+      req(input$pest, input$event, input$type)
+      reacMean <- means[[pests$layer[pests$spp %in% input$pest &
+                                       pests$event %in% input$event &
+                                       pests$type %in% input$type]]]
+      reacDOY <- doys[[pests$layer[pests$spp %in% input$pest &
+                                     pests$event %in% input$event &
+                                     pests$type %in% input$type]]]
+      reacSD <- sds[[pests$layer[pests$spp %in% input$pest &
+                                   pests$event %in% input$event &
+                                   pests$type %in% input$type]]]
+    } else {
+      req(input$threshold)
+      reacMean <- means[[paste0("t", input$threshold)]]
+      reacDOY <- doys[[paste0("t", input$threshold)]]
+      reacSD <- sds[[paste0("t", input$threshold)]]
+    }
+
+    # Determine whether any locations have met threshold
+    reacDOY_NA <- ifelse(ncell(reacDOY) == freq(reacDOY, value = NA), TRUE, FALSE)
     
-    # Mean map
-    # (creating legend that encompasses mean and current-year values)
-    pal_mean <- colorNumeric(palette = "viridis", 
-                             domain = c(values(reacMean()), values(reacDOY())),
+    # Add note to Current DOY panel if no locations have reached threshold yet
+    output$doy_header <- renderText({
+      ifelse(reacDOY_NA == TRUE,
+             "Current day of year - NO LOCATIONS REACHED THRESHOLD YET",
+             "Current day of year")
+    })
+    
+    # Add rasters to each base map (using same legend for both DOY maps)
+    pal_mean <- colorNumeric(palette = "viridis",
+                             domain = c(values(reacMean), values(reacDOY)),
                              na.color = "transparent",
                              reverse = TRUE)
+    
     leafletProxy("mean") %>%
       clearImages() %>%
       clearControls() %>%
-      addRasterImage(reacMean(), 
-                     colors = pal_mean, 
+      addRasterImage(reacMean,
+                     colors = pal_mean,
                      group = "Mean DOY",
                      layerId = "Mean DOY",
-                     opacity = input$opacity, 
+                     opacity = 0.8,
                      project = FALSE) %>%
-      addLegend("bottomright", 
-                pal = pal_mean, 
-                values = c(values(reacMean()), values(reacDOY())),
+      addLegend("bottomright",
+                pal = pal_mean,
+                values = c(values(reacMean), values(reacDOY)),
                 labFormat = myLabelFormat(dates = TRUE),
-                title = "Day of year", 
+                title = "Day of year",
                 opacity = 0.8) %>%
-      addImageQuery(reacMean(),
+      addImageQuery(reacMean,
                     digits = 2,
                     type = "click",
                     position = "bottomleft",
                     prefix = "",
                     layerId = "Mean DOY",
-                    project = TRUE)   
+                    project = TRUE)  
     
-    # SD map
-    pal_sd <- colorNumeric(palette = "plasma",
-                           domain = values(reacSD()),
-                           na.color = "transparent",
-                           reverse = TRUE)
-    leafletProxy("sd") %>%
-      clearImages() %>%
-      clearControls() %>%
-      addRasterImage(reacSD(), 
-                     colors = pal_sd, 
-                     group = "SD",
-                     layerId = "SD",
-                     opacity = input$opacity, 
-                     project = FALSE) %>%
-      addLegend("bottomright", 
-                pal = pal_sd, 
-                values = values(reacSD()),
-                labFormat = myLabelFormat(dates = FALSE),
-                title = "Days", 
-                opacity = 0.8) %>%
-      addImageQuery(reacSD(),
-                    digits = 2,
-                    type = "click",
-                    position = "bottomleft",
-                    prefix = "",
-                    layerId = "SD",
-                    project = TRUE)       
-    
-    # Current DOY map (if at least some locations have reached threshold)
-    if (!reacDOY_NA()) {
+    if (!reacDOY_NA) {
       leafletProxy("doy") %>%
         clearImages() %>%
         clearControls() %>%
-        addRasterImage(reacDOY(), 
+        addRasterImage(reacDOY, 
                        colors = pal_mean, 
                        group = "Current DOY",
                        layerId = "Current DOY",
@@ -281,24 +322,51 @@ server <- shinyServer(function(input, output) {
                        project = FALSE) %>%
         addLegend("bottomright", 
                   pal = pal_mean, 
-                  values = c(values(reacMean()), values(reacDOY())),
+                  values = c(values(reacMean), values(reacDOY)),
                   labFormat = myLabelFormat(dates = TRUE),
                   title = "Day of year", 
                   opacity = 0.8) %>%
-        addImageQuery(reacDOY(),
+        addImageQuery(reacDOY,
                       digits = 2,
                       type = "click",
                       position = "bottomleft",
                       prefix = "",
                       layerId = "Current DOY",
-                      project = TRUE)
+                      project = TRUE)  
     } else {
       leafletProxy("doy") %>%
         clearImages() %>%
         clearControls()
     }
-
-  }) # end observe
+    
+    pal_sd <- colorNumeric(palette = "plasma",
+                           domain = values(reacSD),
+                           na.color = "transparent",
+                           reverse = TRUE)
+    leafletProxy("sd") %>%
+      clearImages() %>%
+      clearControls() %>%
+      addRasterImage(reacSD, 
+                     colors = pal_sd, 
+                     group = "SD",
+                     layerId = "SD",
+                     opacity = input$opacity, 
+                     project = FALSE) %>%
+      addLegend("bottomright", 
+                pal = pal_sd, 
+                values = values(reacSD),
+                labFormat = myLabelFormat(dates = FALSE),
+                title = "Days", 
+                opacity = 0.8) %>%
+      addImageQuery(reacSD,
+                    digits = 2,
+                    type = "click",
+                    position = "bottomleft",
+                    prefix = "",
+                    layerId = "SD",
+                    project = TRUE)  
+    
+  }) # end observeEvent
 }) # end server
 
 # run app ---------------------------------------------------------------------#
