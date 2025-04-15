d5096785b9ee7b60f81af3e5c4f243f1d4465c97
Erin Zylstra <erinzylstra@gmail.com>
2025-04-15 14:35:34 -0700

    Add comparison of rasters calculated with BE and SA methods

--- count-years-exploration.R
+++ count-years-exploration.R
@@ -93,5 +93,67 @@ df2 <- data.frame(comb2) %>%
     count %in% 2:30 ~ "2:30",
   ))
 count(df2, mean2, sd2, count2)
-
 # Everything looks good.
+
+# Compare BE rasters to those calculated using simple averaging----------------#
+sa_thresholds <- c(50, 1250, 2500)
+
+# Load SA rasters into a list 
+means_sa_list <- list()
+sds_sa_list <- list()
+counts_sa_list <- list()
+
+for (i in 1:length(sa_thresholds)) {
+  means_sa_list[[i]] <- rast(paste0("rasters-simple-averaging/normals_mean_simple_", 
+                                    sa_thresholds[i], ".tiff"))
+  sds_sa_list[[i]] <- rast(paste0("rasters-simple-averaging/normals_sd_simple_", 
+                                  sa_thresholds[i], ".tiff"))
+  counts_sa_list[[i]] <- rast(paste0("rasters-simple-averaging/normals_count_simple_", 
+                                     sa_thresholds[i], ".tiff"))
+}
+
+# To compare mean values between BE, SA
+be_thresh_index <- which(thresholds %in% sa_thresholds)
+diff <- list()
+for (thresh in 1:3) {
+  bet <- be_thresh_index[thresh]
+  comb <- c(means_list[[bet]], means_sa_list[[thresh]])
+  diff[[thresh]] <- comb[[1]] - comb[[2]]
+  # title_gg <- ggplot() +
+  #   labs(title = paste0("Threshold: ", sa_thresholds[thresh], " degF"))
+  # be <- ggplot() +
+  #   geom_spatraster(data = comb[[1]], aes(fill = mean)) +
+  #   scale_fill_viridis_c(name = "BE")
+  # sa <- ggplot() + 
+  #   geom_spatraster(data = comb[[2]], aes(fill = mean)) + 
+  #   scale_fill_viridis_c(name = "SA")
+  # plots <- plot_grid(be, sa, nrow = 2)
+  # plot(plot_grid(title_gg, plots, ncol = 1, rel_heights = c(0.1, 1)))
+}
+# Look similar
+
+# Look at differences
+diff[[1]][diff[[1]] == -Inf] <- NA
+diff[[2]][diff[[2]] == -Inf] <- NA
+diff[[3]][diff[[3]] == -Inf] <- NA
+
+ggplot() + 
+  geom_spatraster(data = diff[[1]], aes(fill = mean)) + 
+  scale_fill_viridis_c(name = "BE - SA", na.value = "transparent") + 
+  labs(title = "50 deg")
+global(diff[[1]], fun = "mean", na.rm = TRUE) # Mean = -16
+global(diff[[1]], fun = "sd", na.rm = TRUE) # SD = 5
+
+ggplot() + 
+  geom_spatraster(data = diff[[2]], aes(fill = mean)) + 
+  scale_fill_viridis_c(name = "BE - SA", na.value = "transparent") + 
+  labs(title = "1250 deg")
+global(diff[[2]], fun = "mean", na.rm = TRUE) # Mean = -5
+global(diff[[2]], fun = "sd", na.rm = TRUE) # SD = 1.6
+
+ggplot() + 
+  geom_spatraster(data = diff[[3]], aes(fill = mean)) + 
+  scale_fill_viridis_c(name = "BE - SA", na.value = "transparent") + 
+  labs(title = "2500 deg")
+global(diff[[3]], fun = "mean", na.rm = TRUE) # Mean = -4
+global(diff[[3]], fun = "sd", na.rm = TRUE) # SD = 4.6
